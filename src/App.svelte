83093681a8d021e13ef344b9c1d4e83cb59ff614
Glory Katende <katendeglory@gmail.com>
2023-01-17 16:25:28 +0200

    YAC

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+	import viewport from './useViewportAction';
+</script>
+
+<div style="height: 200px; background: red;" />
+<div style="height: 200px; background: blue;" />
+<div style="height: 200px; background: green;" />
+<div style="height: 200px; background: red;" />
+<div style="height: 200px; background: blue;" />
+<div style="height: 200px; background: green;" />
+
+<h1 use:viewport
+		on:enterViewport={() => console.log('enter!')}
+		on:exitViewport={() => console.log('exit!')}
+>
+	Hello World!
+</h1>
+
+<div style="height: 200px; background: red;" />
+<div style="height: 200px; background: blue;" />
+<div style="height: 200px; background: green;" />
+<div style="height: 200px; background: red;" />
+
+<img
+		 use:viewport
+		 on:enterViewport={(e) => e.target.classList.add("blur-in")}
+		 on:exitViewport={(e) => e.target.classList.remove("blur-in")}
+		 src="/images/camera.gif"
+     class="opacity-0"
+     alt=""
+/>
+
+<div style="height: 200px; background: blue;" />
+<div style="height: 200px; background: green;" />
+<div style="height: 200px; background: red;" />
+<div style="height: 200px; background: blue;" />
+<div style="height: 200px; background: green;" />
---
+++ src/README.md
@@ -0,0 +1,15 @@
+# `use:viewport`
+
+= https://youtu.be/1SKKzdHVvcI
+
+=  Related Resources
+- Tutorials https://svelte.dev/tutorial/actions
+- IntersectionObserver API https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+
+=  Want to learn more from me? Check out these links: 
+
+- Twitter: https://twitter.com/lihautan
+- Blog: https://lihautan.com/
+- Newsletter: https://lihautan.com/newsletter
+
+#svelte #svelteactions
